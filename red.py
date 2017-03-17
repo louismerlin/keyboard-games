18dfdce4307e0f678f9a46fa4bccb51e31c4ff69
Louis Merlin <louis.merlin@epfl.ch>
2017-03-17 23:13:18 +0100

    Add simple script from the logipy page

---
+++ red.py
@@ -0,0 +1,9 @@
+from logipy import logi_led
+import time
+import ctypes
+
+logi_led.logi_led_init()
+time.sleep(1) # Give the SDK a second to initialize
+logi_led.logi_led_set_lighting(100, 0, 0)
+logi_led.logi_led_shutdown()
+
