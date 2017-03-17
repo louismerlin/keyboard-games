0962d9bc171f6a51c03dab57acc61f9cc45632d3
Louis Merlin <louis.merlin@epfl.ch>
2017-03-18 00:25:54 +0100

    Add missing word

--- logishort.py
+++ logishort.py
@@ -4,7 +4,7 @@ def init():
     logi_led.logi_led_init()
 
 def all(r, g, b):
-    logi_led.logi_led_lighting(r, g, b)
+    logi_led.logi_led_set_lighting(r, g, b)
 
 def one(k, r, g, b):
     logi_led.logi_led_lighting_for_key_with_scan_code(k, r, g, b)
