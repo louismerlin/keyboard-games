6fa6603f0ebfef606914d283a46cedab5026b97c
Louis Merlin <louis.merlin@epfl.ch>
2017-03-18 00:23:36 +0100

    Make hello use logishort

--- hello.py
+++ hello.py
@@ -1,27 +1,20 @@
-from logipy import logi_led
+from logishort import *
 import time
-import ctypes
 
-logi_led.logi_led_init()
+init()
 time.sleep(1) # Give the SDK a second to initialize
 
-logi_led.logi_led_set_lighting(17, 28, 47)
+all(17, 28, 47)
 
-logi_led.logi_led_set_lighting_for_key_with_scan_code(logi_led.T, 0, 0, 0)
-logi_led.logi_led_set_lighting_for_key_with_scan_code(logi_led.Y, 0, 0, 0)
-logi_led.logi_led_set_lighting_for_key_with_scan_code(logi_led.U, 0, 0, 0)
-logi_led.logi_led_set_lighting_for_key_with_scan_code(logi_led.G, 0, 0, 0)
-logi_led.logi_led_set_lighting_for_key_with_scan_code(logi_led.H, 0, 0, 0)
-logi_led.logi_led_set_lighting_for_key_with_scan_code(logi_led.J, 0, 0, 0)
-logi_led.logi_led_set_lighting_for_key_with_scan_code(logi_led.B, 0, 0, 0)
-logi_led.logi_led_set_lighting_for_key_with_scan_code(logi_led.N, 0, 0, 0)
-logi_led.logi_led_set_lighting_for_key_with_scan_code(logi_led.M, 0, 0, 0)
-
-
-
-
-
+one(logi_led.T, 0, 0, 0)
+one(logi_led.Y, 0, 0, 0)
+one(logi_led.U, 0, 0, 0)
+one(logi_led.H, 0, 0, 0)
+one(logi_led.J, 0, 0, 0)
+one(logi_led.B, 0, 0, 0)
+one(logi_led.N, 0, 0, 0)
+one(logi_led.M, 0, 0, 0)
 
 input('Press enter to shutdown SDK...')
 
-logi_led.logi_led_shutdown()
+shutdown()
--- logishort.py
+++ logishort.py
@@ -8,3 +8,6 @@ def all(r, g, b):
 
 def one(k, r, g, b):
     logi_led.logi_led_lighting_for_key_with_scan_code(k, r, g, b)
+
+def shutdown():
+    logi_led.logi_led_shutdown()
