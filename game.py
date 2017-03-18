4844f6c361ecfb926bb792d87ec0993ca233b809
Hugo Roussel <Hugo Roussel>
2017-03-18 01:16:11 +0100

    Game.py testage pour char

---
+++ game.py
@@ -0,0 +1,29 @@
+from logishort import *
+from getch import *
+from logipy import logi_led
+import time
+
+init()
+time.sleep(1) # Give the SDK a second to initialize
+
+all(17, 28, 47)
+
+one(logi_led.T, 0, 0, 0)
+one(logi_led.Y, 0, 0, 0)
+one(logi_led.U, 0, 0, 0)
+one(logi_led.H, 0, 0, 0)
+one(logi_led.J, 0, 0, 0)
+one(logi_led.B, 0, 0, 0)
+one(logi_led.N, 0, 0, 0)
+one(logi_led.M, 0, 0, 0)
+one(logi_led.G, 0, 0, 0)
+
+var = input("Entrez un character")
+
+if var==b'\x1b':
+    print("yo")
+
+
+input('Press enter to shutdown SDK...')
+
+shutdown()
